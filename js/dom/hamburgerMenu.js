5ada2dff5e3a78772b6a74be73b04aa220a79641
buchanan77 <buchanan-jonathan77@hotmail.com>
2022-04-21 17:01:35 -0300

    solucion de error de hamburguesa

--- js/dom/hamburgerMenu.js
+++ js/dom/hamburgerMenu.js
@@ -1,9 +1,15 @@
-export default function hamburgerMenu(panelBtn, panel){
+export default function hamburgerMenu(panelBtn, panel, menuLink){
     const d = document;
 
-    document.addEventListener("click", e =>{
-        if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn}*`)){
-            document.querySelector(panel).classList.toggle("is-active");
+    d.addEventListener("click", e =>{
+        if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)){
+            d.querySelector(panel).classList.toggle("is-active");
+            d.querySelector(panelBtn).classList.toggle("is-active");
         }
+
+        if(e.target.matches(menuLink)){
+            d.querySelector(panel).classList.remove("is-active");
+            d.querySelector(panelBtn).classList.remove("is-active");
+    }
     });
 };
